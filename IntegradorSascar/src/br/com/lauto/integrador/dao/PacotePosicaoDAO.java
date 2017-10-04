ea4b1ef40e6555a826295f7e8d84954890618bc6
Filipe Viana Monteiro <filipeviana_@live.com>
2017-10-03 23:20:37 -0300

    Problema nos campos de data
    Data não parece estar certo...

--- IntegradorSascar/src/br/com/lauto/integrador/dao/PacotePosicaoDAO.java
+++ IntegradorSascar/src/br/com/lauto/integrador/dao/PacotePosicaoDAO.java
@@ -6,6 +6,7 @@ import br.com.lauto.integrador.db.DBManager;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Date.*;
 
 public class PacotePosicaoDAO {
     
@@ -18,26 +19,51 @@ public class PacotePosicaoDAO {
     }
     
     public static boolean insertPacotePosicao(PacotePosicao pacote){
-        try {
-            String query = "INSERT INTO pacotePosicoes (idPacote, idVeiculo, latitude, longitude) VALUES (?,?,?,?)";
-            PreparedStatement ps = conn.prepareStatement(query);
-            
-            ps.setLong(1, pacote.getIdPacote());
-            ps.setInt(2, pacote.getIdVeiculo());
-            ps.setDouble(3, pacote.getLatitude());
-            ps.setDouble(4, pacote.getLongitude());
-            
-            ps.executeUpdate();
-            ps.close();
-            
+        if (!checkPacotePosicaoByID(pacote.getIdPacote())){
+            try {
+                String query = "INSERT INTO pacotePosicoes (idPacote, idVeiculo, latitude, longitude, rua, cidade, uf, ignicao, velocidade, bloqueio, dataPosicao, dataPacote) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+
+                ps.setLong(1, pacote.getIdPacote());
+                ps.setInt(2, pacote.getIdVeiculo());
+                ps.setDouble(3, pacote.getLatitude());
+                ps.setDouble(4, pacote.getLongitude());
+                
+                ps.setString(5, pacote.getRua());
+                ps.setString(6, pacote.getCidade());
+                ps.setString(7, pacote.getUf());
+                
+                ps.setInt(8, pacote.getIgnicao());
+                ps.setInt(9, pacote.getVelocidade());
+                ps.setInt(10, pacote.getBloqueio());
+                
+                ps.setTimestamp(11, new Timestamp(pacote.getDataPosicao().getYear(), pacote.getDataPosicao().getMonth(), pacote.getDataPosicao().getDay(),pacote.getDataPosicao().getHour(), pacote.getDataPosicao().getMinute(), pacote.getDataPosicao().getSecond(), pacote.getDataPosicao().getMillisecond()));
+                ps.setTimestamp(12, new Timestamp(pacote.getDataPacote().getYear(), pacote.getDataPacote().getMonth(), pacote.getDataPacote().getDay(), pacote.getDataPacote().getHour(), pacote.getDataPacote().getMinute(), pacote.getDataPacote().getSecond(), pacote.getDataPacote().getMillisecond()));
+                
+                ps.executeUpdate();
+                ps.close();
+
+                return true;
+            } catch (SQLException ex) {
+                return false;
+            }
+        }
+        else{
+            System.out.println("Pacote Repetido");
             return true;
-        } catch (SQLException ex) {
-            return false;
         }
     }
     
-    public static boolean checkPacotePosicaoByID(int id){
-        return true;
+    public static boolean checkPacotePosicaoByID(long id){
+        try{
+            String query = "SELECT * FROM pacotePosicoes WHERE idPacote=" + id;
+            ResultSet rs = st.executeQuery(query);
+            
+            //isBeforeFirst retorna false se ResultSet for vazio e true se não
+            return rs.isBeforeFirst();
+        }catch (SQLException ex){
+            return false;
+        }
     }
     
 }
